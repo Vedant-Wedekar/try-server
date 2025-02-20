06db51c1d1c734a9ea741101686ea7116d824dc1
VEDANT WEDEKAR <vedantwedekar@gmail.com>
2025-02-20 05:50:09 +0000

    basic npm set up for server

--- contro/con.js
+++ contro/con.js
@@ -1,5 +1,4 @@
 const home = (req,res)=>{
     res.status(200).send('Hell111111111111o World');
 }
-
 module.exports ={ home};
--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.use('/auth',route);
 // app.get('/',(req,res)=>(
 //     res.status(200).send('Hello World')
 // ))
-app.listen(3000,()=>console.log('Server is running on port 3000'));
+app.listen(5173,()=>console.log('Server is running on port 3000'));
